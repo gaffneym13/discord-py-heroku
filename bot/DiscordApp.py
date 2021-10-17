9cc195ef99f260d2bc67794e8a4d5ff73b55fecd
gaffneym13 <45971900+gaffneym13@users.noreply.github.com>
2021-10-17 14:01:31 +0100

    Update DiscordApp.py

--- bot/DiscordApp.py
+++ bot/DiscordApp.py
@@ -2,8 +2,6 @@ import os
 import discord
 import string
 
-
-
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
 
@@ -15,14 +13,13 @@ translator = str.maketrans('', '', string.punctuation)
 def response_decider(message):
     sentence = message.translate(translator)
     for word in sentence.split():
-        if word.endswith('er') :
+        if (word.endswith('er')) & (word != 'er') & (word != 'her'):
             response = word.title() + '? I barely knew her!'
-            return(response)
+            return response
         if (word.endswith('im')) & (word != 'im') & (word != 'him'):
             response = word.title() + '? I barely knew him!'
-            return(response)
+            return response
     return
-
 
 
 @client.event
